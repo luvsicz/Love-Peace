b5a101e0bbbcef462f0475322451fd641c7405cb
luvsic <alience98@gmail.com>
2020-04-15 14:55:51 +0800

    404,500,以及其他异常处理优化.

--- hrm/hrm-web/src/main/java/org/dev/hrm/exception/GlobalExceptionHandler.java
+++ hrm/hrm-web/src/main/java/org/dev/hrm/exception/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package org.dev.hrm.exception;
 
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.dev.hrm.model.RespBean;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
@@ -13,14 +16,51 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  * @createTime 2020年03月27日 17:32:00
  */
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
-  //处理SQL的异常
+  /**
+   * 处理SQL的异常
+   */
   @ExceptionHandler(SQLException.class)
   public RespBean constraintViolationException(SQLException e) {
     if (e instanceof SQLIntegrityConstraintViolationException) {
+      log.error("异常:本次操作违反外键约束，操作失败!", e);
       return RespBean.error("本次操作违反外键约束，操作失败!");
     }
     return RespBean.error("数据库异常!");
+  }
+
+  /**
+   * 处理空指针的异常
+   */
+  @ExceptionHandler(value = NullPointerException.class)
+  @ResponseBody
+  public RespBean exceptionHandler(NullPointerException e) {
+    log.error("异常:空指针异常", e);
+    return RespBean.error("空指针异常");
+  }
+
+  /**
+   * 处理请求方式不合法的异常
+   */
+  @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+  @ResponseBody
+  public RespBean exceptionHandler(HttpRequestMethodNotSupportedException e) {
+    log.error("异常:请求方式不合法", e);
+    return RespBean.error("请求方式不合法");
+  }
+
+
+  /**
+   * 处理其他异常 404 这里无法处理
+   * 不仅仅404异常，Servlet Filter中的异常@ExceptionHandler也处理不了， @ExceptionHandler的处理器是一个HandlerExceptionResolver的实现类，
+   * 而HandlerExceptionResolver处理的是DispatcherServlet处理请求中的异常，所以没到Servlet的在Filter中的异常使用@ExceptionHandler也不行
+   */
+  @ExceptionHandler(value = Exception.class)
+  @ResponseBody
+  public RespBean exceptionHandler(Exception e) {
+    log.error("异常:未知异常", e);
+    return RespBean.error("未知异常");
   }
 }
